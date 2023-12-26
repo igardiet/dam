ca6e1a06da7bf96b6cf432b1cf24928e6605bca8
igardiet <igardiet@gmail.com>
2023-12-26 22:49:34 +0100

    feat: ex. 11. input number loop

---
+++ programacion/if-exercises/Eleven.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+// Pedir un número N, y mostrar todos los números del 1 al N.
+public class Eleven
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int number;
+
+        System.out.println("Enter a number:");
+        number = scanner.nextInt();
+
+        System.out.println("Numbers from 1 to " + number + ":");
+
+        if (number > 0)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                System.out.println(i);
+            }
+        } else if (number < 0)
+        {
+            for (int i = 0; i <= Math.abs(number); i++)
+            {
+                System.out.println("-" + i);
+            }
+        } else
+        {
+            System.out.println("There are no numbers to show");
+        }
+        scanner.close();
+    }
+}
