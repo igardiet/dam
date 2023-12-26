8f5a5d77ad97a009fe9e0bc905ab3fd40b55861b
igardiet <igardiet@gmail.com>
2023-12-26 18:22:49 +0100

    feat: assignment operators exercise

---
+++ programacion/assignmentOperators.java
@@ -0,0 +1,21 @@
+public class assignmentOperators
+{
+    public static void main(String[] args)
+    {
+        double saldo = 100.50;
+        System.out.println("Initial balance: " + "€" + saldo);
+
+        saldo += 27.50;
+        saldo *= 2;
+        saldo /= 2;
+        saldo += 250.38;
+        saldo -= 55.37;
+        saldo += 1200.96;
+
+        System.out.println("Current balance: " + "€" + saldo);
+
+        saldo -= 85.23;
+
+        System.out.println("Final balance: " + "€" + saldo);
+    }
+}
