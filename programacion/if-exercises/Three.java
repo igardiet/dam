175de9435e6c92240c1466a5b7725b4af3c1feb7
igardiet <igardiet@gmail.com>
2023-12-26 18:26:20 +0100

    feat: ex. 3. input major or equal numbers

---
+++ programacion/if-exercises/Three.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+// Pedir dos números y decir cual es el mayor o si son iguales.
+public class Three
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Enter the second number:");
+        int number2 = scanner.nextInt();
+
+        if (number1 > number2)
+        {
+            System.out.println(number1 + " is bigger than " + number2);
+        } else if (number2 > number1)
+        {
+            System.out.println(number2 + " is bigger than " + number1);
+        } else
+        {
+            System.out.println("Both numbers are the same one");
+        }
+
+        scanner.close();
+    }
+}
