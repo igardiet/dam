8597630d76408af6ad02ad9a7434c1bf46aefe81
igardiet <igardiet@gmail.com>
2023-12-26 22:51:17 +0100

    feat: ex. 13. calculate factorial

---
+++ programacion/if-exercises/Thirdteen.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+// Pedir un número y calcular su factorial.
+public class Thirdteen
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number to calculate its factorial:");
+        int number = scanner.nextInt();
+
+        long factorial = calculateFactorial(number);
+
+        System.out.println("The factorial of " + number + " is: " + factorial);
+
+        scanner.close();
+    }
+
+    private static long calculateFactorial(int number)
+    {
+        if (number < 0)
+        {
+            System.out.println("The factorial of a negative number cannot be calculated.");
+            return -1;
+        } else if (number == 0 || number == 1)
+        {
+            return 1;
+        } else
+        {
+            long result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
