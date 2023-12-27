fb0ccc37302ade80b0f992f4cee4d1f5cee221bd
igardiet <igardiet@gmail.com>
2023-12-27 18:55:35 +0100

    feat: ex. 20. prime number program

---
+++ programacion/if-exercises/Twenty.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+// Realizar un programa que nos pida un número n, y nos diga cuantos números hay entre 1 y n que son primos.
+public class Twenty
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+
+        int primeCounter = countPrime(number);
+
+        System.out.println("There are " + primeCounter + " prime numbers between 1 and " + number);
+
+        scanner.close();
+    }
+
+    private static boolean isPrime(int num)
+    {
+        if (num < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static int countPrime(int number)
+    {
+        int counter = 0;
+
+        for (int i = 1; i <= number; i++)
+        {
+            if (isPrime(i))
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+}
