c61834569da24431e97672e090c252032d9e29f6
igardiet <igardiet@gmail.com>
2023-12-28 00:20:21 +0100

    feat: greater than program

---
+++ programacion/EstructurasControl1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class EstructurasControl1
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Enter the second number:");
+        int number2 = scanner.nextInt();
+
+        if (number1 > number2)
+        {
+            System.out.println(number1 + " is greater than " + number2);
+            System.out.println(number2 + " is less than " + number1);
+        } else if (number1 < number2)
+        {
+            System.out.println(number2 + " is greater than " + number1);
+            System.out.println(number1 + " is less than " + number2);
+        } else
+        {
+            System.out.println("Both numbers are the same!");
+        }
+
+        scanner.close();
+    }
+}
