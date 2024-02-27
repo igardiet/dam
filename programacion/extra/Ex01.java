55f9cb89bce39f4f1f400de676f7390db15c59ed
igardiet <igardiet@gmail.com>
2024-02-27 16:12:22 +0100

    feat: extra exercise 01

---
+++ programacion/extra/Ex01.java
@@ -0,0 +1,18 @@
+package extra;
+
+import java.util.Scanner;
+
+public class Ex01
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name + "!");
+
+        scanner.close();
+    }
+}
