806ec6ee0e967b0302a273385b11eaa61b3acf0b
igardiet <igardiet@gmail.com>
2024-03-04 22:31:51 +0100

    update: reordered and formatted code

--- programacion/array/ArrayIndex.java
+++ programacion/array/ArrayIndex.java
@@ -4,33 +4,26 @@ public class ArrayIndex
 {
     public static void main(String[] args)
     {
-        int[] array1 = new int[5]; // Declaración con tamaño fijo
+        // Declaración con tamaño fijo
+        int[] array1 = new int[5];
 
-        int[] array2 = { 1, 2, 3, 4, 5 }; // Init directa con valores
+        // Init directa con valores
+        int[] array2 = { 1, 2, 3, 4, 5 };
 
+        // Declaración e init separadas
         int[] array3;
-        array3 = new int[] { 1, 2, 3, 4, 5 }; // Declaración e init separadas
+        array3 = new int[] { 1, 2, 3, 4, 5 };
 
-        int array4[] = new int[5]; // Declaración con tamaño implícito
+        // Declaración con tamaño implícito
+        int array4[] = new int[5];
 
-        int array5[] = { 1, 2, 3, 4, 5 }; // Declaración con tamaño y valores implícitos
+        // Declaración con tamaño y valores implícitos
+        int array5[] = { 1, 2, 3, 4, 5 };
 
-        // -------------------------------------------------------------------
-
-        // Inicialización con valores conocidos: Si conoces los valores que
-        // deseas almacenar en el array desde el principio, la inicialización
-        // directa con valores es conveniente y fácil de entender.
-        int[] array6 = { 1, 2, 3, 4, 5 };
-        
-        // Declaración e inicialización separadas: Si necesitas declarar el
-        // array primero y luego inicializarlo más tarde en el código, esta
-        // forma es útil.
-        int[] array7;
-        array7 = new int[] { 1, 2, 3, 4, 5 };
-        
-        // Declaración con tamaño fijo: Si necesitas un array de un tamaño
-        // específico pero no conoces los valores exactos en el momento de la
-        // declaración, esta forma es apropiada.
-        int[] array8 = new int[5];
+        System.out.println(array1);
+        System.out.println(array2);
+        System.out.println(array3);
+        System.out.println(array4);
+        System.out.println(array5);
     }
 }
