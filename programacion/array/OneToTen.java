1dc306eef98c39f273e9fb5aade2c8d76f9a16fe
igardiet <igardiet@gmail.com>
2024-03-04 22:33:12 +0100

    feat: OneToTen array exercise

---
+++ programacion/array/OneToTen.java
@@ -0,0 +1,19 @@
+package array;
+
+public class OneToTen
+{
+    public static void main(String[] args)
+    {
+        int array[] = new int[10];
+
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = i + 1;
+        }
+
+        for (int i = array.length - 1; i >= 0; i--)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
