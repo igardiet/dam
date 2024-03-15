a2f9dcddd7ca3c44f17f5876aa0a3bd34cb29c91
igardiet <igardiet@gmail.com>
2024-03-15 16:57:28 +0100

    feat: Ex15 extra exercise

---
+++ programacion/extra/Ex15.java
@@ -0,0 +1,14 @@
+package extra;
+
+public class Ex15
+{
+    public static void main(String[] args)
+    {
+        String[] fruits = { "Kiwi", "Orange", "Coconut", "Apple" };
+
+        for (String fruit : fruits)
+        {
+            System.out.println(fruit);
+        }
+    }
+}
