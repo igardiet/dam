ff32bde76cfcb0600ac59d51c4dc7a6e5c3a7348
igardiet <igardiet@gmail.com>
2024-06-07 12:17:46 +0200

    feat: matrix exercise

---
+++ programacion/ReturnMatrix.java
@@ -0,0 +1,17 @@
+public class ReturnMatrix
+{
+    public static void main(String[] args)
+    {
+        int[][] matrix = new int[3][3];
+
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                matrix[i][j] = (i * matrix.length) + (j + 1);
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
